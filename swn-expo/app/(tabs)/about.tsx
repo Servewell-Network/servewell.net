1fc12502800c6540815bb40ac16c9148479e53e2
Marcus Reese <marcusoccasional@gmail.com>
2025-11-07 22:31:49 -0700

    render first markdown as in https://github.com/EvanBacon/expo-mdx

--- swn-expo/app/(tabs)/about.tsx
+++ swn-expo/app/(tabs)/about.tsx
@@ -1,9 +1,15 @@
 import { Text, View, StyleSheet } from 'react-native';
+import Readme from '../../README.mdx';
+import { MDXStyles } from "@bacons/mdx";
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About screen</Text>
+      <MDXStyles
+      >
+        <Readme />
+      </MDXStyles>
     </View>
   );
 }
@@ -11,11 +17,14 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start', // vertical
+    alignItems: 'flex-start', // horizontal
+    paddingHorizontal: '10%',
+    // backgroundColor: '#222', 
+    // color: '#fff',
+    overflow: 'scroll',
   },
   text: {
-    color: '#fff',
+    color: 'pink',
   },
 });
