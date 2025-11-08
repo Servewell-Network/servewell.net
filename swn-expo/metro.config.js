a46f845855eb86a52e6934fb4532e5925d7e0296
Marcus Reese <marcusoccasional@gmail.com>
2025-11-07 18:17:40 -0700

    oops this file is for next commit

--- swn-expo/metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withMdx } = require("@bacons/mdx/metro");
-
-const config = withMdx(getDefaultConfig(__dirname));
-
-module.exports = config;
