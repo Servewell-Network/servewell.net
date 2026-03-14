c5b62a3063d84c6e60c6d40eb6bef87fd7a68bd8
Marcus Reese <marcusoccasional@gmail.com>
2026-03-14 17:29:34 -0600

    delete expo

--- swn-expo/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withMdx } = require("@bacons/mdx/metro");
-
-const config = withMdx(getDefaultConfig(__dirname));
-console.log('dirname', __dirname);
-
-module.exports = config;
-
